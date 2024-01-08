fa251646c7f2a3b6707656560506159969d84a87
conmecto <conmecto@gmail.com>
2024-01-08 19:21:45 +0530

    fix: launch logo screen

--- src/screens/logo.tsx
+++ src/screens/logo.tsx
@@ -1,13 +1,29 @@
 import React, { useEffect } from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, StyleSheet, Text, Dimensions } from 'react-native';
 import { IMAGE_LOGO } from '../files';
+import { COLOR_CODE } from '../utils/enums';
+
+const { height, width } = Dimensions.get('window');
 
 const LogoScreen = ({ navigation }: any) => {  
   return (
-    <View>
-      <Image source={ IMAGE_LOGO } />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Image source={ IMAGE_LOGO } style={styles.logo}  />
+      <Text style={styles.text}>Conmecto</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    height: height * 0.15,
+    width: height * 0.15
+  },
+  text: {
+    fontWeight: 'bold',
+    fontFamily: 'SavoyeLetPlain',
+    fontSize: 50
+  }
+});
 
 export default LogoScreen;
