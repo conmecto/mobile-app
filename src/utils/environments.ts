61540b7fbb8229c6520b85c38223043b8756fab2
conmecto <conmecto@gmail.com>
2024-08-17 14:30:19 +0530

    fix: removed country code from api

--- src/utils/environments.ts
+++ src/utils/environments.ts
@@ -1,21 +1,19 @@
-import Config from 'react-native-config'; 
-import { addCountryToApi } from './user.country';
-
+import Config from 'react-native-config';
 export default {
   api: {
     userService: {
-      baseUrl: addCountryToApi(Config.API_BASE_URL_USER_SERVICE) || ''
+      baseUrl: Config.API_BASE_URL_USER_SERVICE || ''
     },
     matchService: {
-      baseUrl: addCountryToApi(Config.API_BASE_URL_MATCH_SERVICE) || ''
+      baseUrl: Config.API_BASE_URL_MATCH_SERVICE || ''
     },
     profileService: {
-      baseUrl: addCountryToApi(Config.API_BASE_URL_PROFILE_SERVICE) || ''
+      baseUrl: Config.API_BASE_URL_PROFILE_SERVICE || ''
     }
   },
   socket: {
     matchService: {
-      baseUrl: addCountryToApi(Config.API_BASE_URL_MATCH_SERVICE_CHAT_SOCKET) || ''
+      baseUrl: Config.API_BASE_URL_MATCH_SERVICE_CHAT_SOCKET || ''
     }
   },
   appEnv: Config.APP_ENV || 'dev'
